4a8b890fc70c2ea745d0501aee8a45a1d59bd72d
alliahsrm <alliahsarahmmadrigal@iskolarngbayan.pup.edu.ph>
2026-05-07 14:26:13 +0800

    new file:   Models/Users.js modified:   Models/spocomform.js modified:   index.js

---
+++ Models/Users.js
@@ -0,0 +1,9 @@
+// Models/User.js
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, // In a real app, use bcrypt to hash this!
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- Models/spocomform.js
+++ Models/spocomform.js
@@ -1,13 +1,15 @@
+// Models/spocomform.js
 const mongoose = require("mongoose");
 
-const FormSchema = new mongoose.Schema({
-  spotifyLink: { type: String },
-  albumTitle: { type: String, required: true },
-  artist: { type: String, required: true },
-  favSong: { type: String },
-  rating: { type: Number, required: true },
-  comment: { type: String },
-  cover: { type: String }
-}, { timestamps: true });
+const AlbumSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  spotifyLink: String,
+  albumTitle: String,
+  artist: String,
+  favSong: String,
+  rating: Number,
+  comment: String,
+  cover: String
+});
 
-module.exports = mongoose.model("Album", FormSchema);
+module.exports = mongoose.model("Album", AlbumSchema);
